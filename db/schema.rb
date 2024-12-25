facc04c2a8860166f13054dc5125c89a42c1e7ac
Takuma Kira <takuma@kirakiraworx.com>
2024-12-26 01:25:51 +0900

    [dev]: Generate Workout model
    bin/rails generate model Workout name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_25_161433) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_25_162203) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -26,6 +26,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_25_161433) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "sessions", "users"
