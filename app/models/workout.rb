af81ca3acec31aa3d1f8ce85a31d8e9743a1f3e2
Takuma Kira <takuma@kirakiraworx.com>
2024-12-26 13:14:53 +0900

    [dev]: Add link between Workout and Exercise

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :workout_exercises, dependent: :destroy
+  has_many :exercises, through: :workout_exercises
 
   validates :name, presence: true
   validates :date, presence: true
